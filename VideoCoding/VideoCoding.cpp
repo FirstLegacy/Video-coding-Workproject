971be82353ac006cdf5b23802ae93845951c0e6f
Emil-M <s154202@student.dtu.dk>
2017-03-08 15:04:14 +0100

    test

---
+++ VideoCoding/VideoCoding.cpp
@@ -0,0 +1,20 @@
+// cppExample.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "OtherClass.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	cout << OtherClass::helloWorld() << endl;
+	cout << OtherClass::pointerExampleStack() << endl;
+	cout << OtherClass::pointerExampleHeap() << endl;
+	cout << OtherClass::pointerExampleUnique() << endl;
+	cout << OtherClass::pointerExampleFunc() << endl;
+	cout << OtherClass::arrayExample() << endl;
+}
