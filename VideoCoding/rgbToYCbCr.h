805d5707e41d40fccf9f4136ab7ae7d0ed3c7c08
Mwthorn <mwthorn@gmail.com>
2017-06-02 13:57:51 +0200

    Konverting til Vektore

--- VideoCoding/rgbToYCbCr.h
+++ VideoCoding/rgbToYCbCr.h
@@ -7,6 +7,6 @@ class RgbToYCbCr
 {
 public:
 	static std::vector<char> convert(std::array<uint8_t, img_res_rgb>);
-	void downSampling(std::array<uint8_t, img_res_rgb>& in);
+	static void downSampling(std::vector<uint8_t>, std::vector<uint8_t>, std::vector<uint8_t>, std::vector<uint8_t>*);
 };
 
