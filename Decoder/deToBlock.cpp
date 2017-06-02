ef71b0b692c7225d0ba96fbf9a7e306f83ea39a9
Mwthorn <mwthorn@gmail.com>
2017-06-02 15:26:24 +0200

    Updated but not fully made DeToBlock

--- Decoder/deToBlock.cpp
+++ Decoder/deToBlock.cpp
@@ -1,16 +1,22 @@
+//#include "stdafx.h"
 #include "deToBlock.h"
+#include "DeDCT.h"
 
 #include <vector>
 #include <array>
 
-uint8_t* deBlockify(uint8_t*** blocks) {
+std::vector<uint_fast8_t> deToBlock::deBlockify(std::vector<char> blocks) {
+	std::vector<uint_fast8_t> aList; // Empty stack.
+	// TODO
+	// Remove temp integers
+	int temp_img_res = 9;
+	int temp_mBlockSize = 8;
+	aList.reserve(temp_img_res);
+	std::vector<uint_fast8_t> listBytes;
+	for (int i = 0; i < temp_mBlockSize; ++i)
+		for (int j = 0; j < temp_mBlockSize; ++j)
+			for (int k = 0; k < temp_mBlockSize; ++k)
+				aList.at(temp_mBlockSize * i + temp_mBlockSize * j + k) = blocks.at(i);
 
-	int size = 8;
-	uint8_t* listBytes = new uint8_t[size];
-	for (int i = 0; i < size; ++i)
-		for (int j = 0; j < size; ++j)
-			for (int k = 0; k < size; ++k)
-				listBytes[size*size*i + size*j + k] = blocks[i][j][k];
-
-	return listBytes;
+	return aList;
 }
